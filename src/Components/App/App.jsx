01e6b8db58c8d1dd076cf1e69cd7a1800bfe1902
Mário Travassos <retchut@protonmail.ch>
2022-12-17 21:44:00 +0000

    chore: move App component into the components folder

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,13 @@
+import Footer from '../Footer/Footer.jsx';
+import Navbar from '../Navbar/Navbar.jsx';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+}
+
+export default App;

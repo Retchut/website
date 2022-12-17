881bcf499548d6355b7d007e99fcbbe39c298a4d
Mário Travassos <retchut@protonmail.ch>
2022-12-17 15:45:03 +0000

    feat: configure tailwind and color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-primary": "#081426",
+        "bg-section": "#0e1b33",
+        "text-primary": "#FFFFFF",
+        "text-secondary": "#60ffd9",
+        "bg-tag": "#085a4f",
+        "bg-tech": "#4B0082",
+      },
+    },
   },
   plugins: [],
 }

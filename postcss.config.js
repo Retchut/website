98eeac434c70bf4e4f3bf78962521fd73a225692
Mário Travassos <retchut@protonmail.ch>
2022-12-17 15:37:47 +0000

    feat: setup environment

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.jsx",
+  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      ya like jazz?
+    </div>
+  );
+}
+
+export default App;

00209ecba25346b09d44b0e5430be73edfa8c788
Mário Travassos <retchut@protonmail.ch>
2022-12-24 02:34:00 +0000

    refactor: extract interest showcase into component

--- src/Components/Film/Film.jsx
+++ src/Components/Film/Film.jsx
@@ -1,47 +1,18 @@
-import { capitalize } from '../../Utils/textUtils.js';
-import YoutubeEmbed from '../YoutubeEmbed/YoutubeEmbed.jsx';
+import InterestShowcase from "../InterestShowcase/InterestShowcase";
 
 function getImage(imgName){
     const folderPath = "./Images/Films/";
     return <img className="aspect-poster min-w-full object-fill" src={folderPath + imgName} alt={imgName} />
 }
 
-function getShowcase({ name, director, video, links }){
-    const videoKeys = Object.keys(video);
-    const showVideoLabel = true && (videoKeys.length !== 0);
-    const videoLabel = "Trailer";
-
-    const linkKeys = Object.keys(links);
-    const linkLabel = "See more:";
-
-    return (
-        <div className="background-box mx-4 px-4 h-full flex flex-col justify-center items-center">
-            <div className="my-3 text-center">
-                <p className="mb-2 text-4xl">{name}</p>
-                <p className="my-1 text-xl">Director: {director}</p>
-            </div>
-            {showVideoLabel && <p className="text-xl pb-1">{videoLabel}</p>}
-            <div className="w-full flex flex-col items-center pb-2">
-                {
-                    videoKeys.map((type, index) => {
-                        return <YoutubeEmbed key={index} embedID={video[type]} />
-                    })
-                }
-            </div>
-            <div className="w-full grow flex items-end pb-1">
-                <div className="w-full">
-                    <p className="pl-4 text-xl">{linkLabel}</p>
-                    <div className="flex justify-center">
-                        {linkKeys.map((platform, index) => {
-                            return (
-                                <a className="m-2 link-btn" href={links[platform]}>{capitalize(platform)}</a>
-                            )
-                        })}
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+function getShowcase(data){
+    const showcaseLabels = {
+        subtitleLabel : "Director:",
+        videoLabel : "Trailer",
+        linkLabel : "See more:"
+    }
+    
+    return <InterestShowcase labels={showcaseLabels} data={data} />
 }
 
 function Film({ filmData, showcaseOnRight }){
---
+++ src/Components/InterestShowcase/InterestShowcase.jsx
@@ -0,0 +1,48 @@
+import YoutubeEmbed from '../YoutubeEmbed/YoutubeEmbed.jsx';
+
+import { capitalize } from '../../Utils/textUtils.js';
+
+function InterestShowcase({ labels, data }){
+    const { title, subtitle, video, links } = data;
+    const { subtitleLabel, videoLabel, linkLabel } = labels;
+
+    const videoKeys = Object.keys(video);
+    const showVideoLabel = true && (videoKeys.length !== 0);
+    
+    const linkKeys = Object.keys(links);
+
+    return (
+        <div className="background-box mx-4 px-4 h-full flex flex-col justify-center items-center">
+            <div className="my-3 text-center">
+                <p className="mb-2 text-4xl">{title}</p>
+                <p className="my-1 text-xl">{subtitleLabel} {subtitle}</p>
+            </div>
+            {showVideoLabel && <p className="text-xl pb-1">{videoLabel}</p>}
+            <div className="w-full flex flex-col items-center pb-2">
+                {
+                    videoKeys.map((type, index) => {
+                        return (
+                            <div className="pb-1">
+                                <YoutubeEmbed key={index} embedID={video[type]} />
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div className="w-full grow flex items-end pb-1">
+                <div className="w-full">
+                    <p className="pl-4 text-xl">{linkLabel}</p>
+                    <div className="flex justify-center">
+                        {linkKeys.map((platform, index) => {
+                            return (
+                                <a className="m-2 link-btn" href={links[platform]}>{capitalize(platform)}</a>
+                            )
+                        })}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default InterestShowcase;
